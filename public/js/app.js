8703a9c19a3b40c5634578a2949f94f19f6db886
Arnaud Melin <arnaudmelin@hotmail.com>
2017-04-14 14:52:35 +0200

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 var socket = io();
-var now = moment();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 
 socket.on('connect', function () {
     console.log('Connected to socket.io server!');
@@ -7,11 +8,13 @@ socket.on('connect', function () {
 
 socket.on('message', function (message) {
     var timestampMoment = moment.utc(message.timestamp);
-    
+    var $message = jQuery('.messages');
+        
     console.log('New message:');
     console.log(message.text);
     
-    jQuery('.messages').append('<p><strong>' + timestampMoment.local().format('h:mm a') + ' - </strong> ' + message.text + '</p>');
+    $message.append('<p><strong>' + message.name + ' ' + timestampMoment.local().format('h:mm a') + '</strong></p>');
+    $message.append('<p>' + message.text + '</p>');
 });
 
 // Handles submitting of new message
@@ -23,6 +26,7 @@ $form.on('submit', function (event) {
     var $message = $form.find('input[name=message]');
     
     socket.emit('message', {
+        name: name,
         text: $message.val()
     });
     
